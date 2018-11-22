81d942f2177db04067346c438c99d41fa009ed55
buxizhizhoum <mapeaks@126.com>
2018-11-22 22:50:15 +0800

    Add subsets.py

---
+++ subsets.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Given a set of distinct integers, nums, return all possible subsets (the power set).
+
+Note: The solution set must not contain duplicate subsets.
+
+Example:
+
+Input: nums = [1,2,3]
+Output:
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+"""
+
+
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        answer = []
+        self.find_subsets(res, answer, nums)
+        res.append([])
+        return res
+
+    def find_subsets(self, res, answer, nums):
+        if len(nums) == 0:
+            return
+
+        for i in range(len(nums)):
+            current_num = nums[i]
+
+            res.append(answer + [current_num])
+            self.find_subsets(res, answer + [current_num], nums[i+1:])
+
+
+if __name__ == "__main__":
+    test_nums = [1, 2, 3]
+    print(Solution().subsets(test_nums))
