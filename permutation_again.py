5e67b9d21a1c6d8fe1562bb9f968113dafb838b3
buxizhizhoum <mapeaks@126.com>
2018-12-15 19:55:00 +0800

    Add backtracking method

--- permutation_again.py
+++ permutation_again.py
@@ -27,7 +27,8 @@ class Solution(object):
         res = []
         answer = []
         length = len(nums)
-        self.find_permute(nums, res, answer, length)
+        # self.find_permute(nums, res, answer, length)
+        res = self.backtracking(nums)
         return res
 
     def find_permute(self, nums, res, answer, length):
@@ -40,6 +41,59 @@ class Solution(object):
             current_num = nums[i]
             # nums[:i] + nums[i+1:] is the smaller list after num[i] is removed
             self.find_permute(nums[:i] + nums[i+1:], res, [current_num] + answer, length)
+
+    # def backtracking(self, nums):
+    #     # cache = [[] for _ in range(len(nums))]
+    #     cache = []
+    #     for i in range(len(nums)):
+    #         cache.append([])
+    #         for j in range(len(nums)):
+    #             cache[i].append([])
+    #     # initialize base condition
+    #     for i in range(len(nums)):
+    #         for j in range(len(nums)):
+    #             if i == 0:
+    #                 cache[i][j] = [[nums[i]]]
+    #             if j == 0 and i != 0:
+    #                 for digit in nums[:i+1]:
+    #                     cache[i][j].append([digit])
+    #
+    #     for i in range(1, len(nums)):
+    #         for j in range(1, len(nums)):
+    #
+    #             for ans in cache[i-1][j-1]:
+    #                 insert nums[i] from start to end in every ans in cache[i-1][j-1]
+    #                 for index in range(len(ans) + 1):
+    #                     tmp = ans[::]
+    #                     tmp.insert(index, nums[i])
+    #                     cache[i][j].append(tmp)
+    #
+    #     return cache[len(nums)-1][len(nums)-1]
+
+    def backtracking(self, nums):
+        cache = []
+        # initialize base condition, buffer result in a 2D list
+        for i in range(len(nums)):
+            cache.append([])
+            for j in range(len(nums)):
+                cache[i].append([])
+                if i == 0:
+                    cache[i][j].append([nums[i]])
+                if j == 0 and i != 0:
+                    for digit in nums[:i+1]:
+                        cache[i][j].append([digit])
+
+        for i in range(1, len(nums)):
+            for j in range(1, len(nums)):
+                # todo: Attention this is the core logic to solve this problem
+                for ans in cache[i-1][j-1]:
+                    # insert nums[i] from start to end in every ans in cache[i-1][j-1]
+                    # considering insert is more expensive, use concat instead
+                    for index in range(len(ans) + 1):
+                        new_ans = ans[index:] + [nums[i]] + ans[:index]
+                        cache[i][j].append(new_ans)
+
+        return cache[len(nums)-1][len(nums)-1]
 
 
 if __name__ == "__main__":
