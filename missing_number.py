a4c96a485991b6482624d920a92b0228cb316d9f
buxizhizhoum <mapeaks@126.com>
2019-01-02 14:48:14 +0800

    Add missing_number.py

---
+++ missing_number.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+
+Example 1:
+
+Input: [3,0,1]
+Output: 2
+Example 2:
+
+Input: [9,6,4,2,3,5,7,0,1]
+Output: 8
+"""
+from collections import Counter
+
+
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return self.ans(nums)
+
+    def ans(self, nums):
+        count = Counter(nums)
+
+        for i in range(len(nums)+1):
+            if count.get(i) != 1:
+                return i
+
+
+if __name__ == "__main__":
+    test_nums = range(10) + range(11, 17)
+    print(Solution().missingNumber(test_nums))
+
+
