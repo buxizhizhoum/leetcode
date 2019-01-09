c8bf1a81435a5e771305104cb0989f37fac61694
buxizhizhoum <mapeaks@126.com>
2019-01-09 09:43:18 +0800

    Add another method with new thought

--- partition_equal_subset_sum.py
+++ partition_equal_subset_sum.py
@@ -34,30 +34,72 @@ class Solution(object):
         if sum_all % 2 != 0:  # no way to partition sum to two part
             return False
 
-        # return self.recursion(nums, sum_all)
-        return self.dp(nums)
+        return self.recursion(nums, sum_all)
+        # return self.dp(nums)
 
     def recursion(self, nums, sum_all):
         # return self._recursion(nums, len(nums)-1, sum_all//2)
+        # return self._recursion_1(nums, 0, sum_all//2)
 
         # len(nums) is the count of rows and sum_all // 2 + 1 is the col count
         memo = [[None for j in range(sum_all//2+1)] for _ in range(len(nums))]
-        return self._recursion_memo(nums, len(nums)-1, sum_all//2, memo)
+        # return self._recursion_memo(nums, len(nums)-1, sum_all//2, memo)
+        return self._recursion_1_memo(nums, 0, sum_all//2, memo)
 
-    def _recursion(self, nums, index, sum_total):
+    # def _recursion(self, nums, index, sum_total):
+    #     # this also works
+    #     if sum_total == 0:
+    #         return True
+    #
+    #     if index < 0 or sum_total < 0:
+    #         # could not fill the package
+    #         return False
+    #     # todo: usage of index, how about a for loop?
+    #     # 0 or 1, two choices
+    #     # res = False
+    #     # for x in (0, 1):
+    #     #     res = res or self._recursion(nums, index-1, sum_total - index * x)
+    #     return self._recursion(nums, index-1, sum_total) or \
+    #                 self._recursion(nums, index-1, sum_total - nums[index])
+
+    def _recursion_1(self, nums, index, sum_total):
         if sum_total == 0:
             return True
 
-        if index < 0 or sum_total < 0:
+        if index >= len(nums) or sum_total < 0:
             # could not fill the package
             return False
         # todo: usage of index, how about a for loop?
-        # 0 or 1, two choices
-        # res = False
-        # for x in (0, 1):
-        #     res = res or self._recursion(nums, index-1, sum_total - index * x)
-        return self._recursion(nums, index-1, sum_total) or \
-                    self._recursion(nums, index-1, sum_total - nums[index])
+        # how this kind of recursion consider put and not put situation?
+        # this case, nums[i] is put into
+        # if not possible, at next case, nums[i] is skipped
+        # todo: try to draw recursion tree
+        # 0-1 package is 2 situation, relation to for loop
+        # according to recursion tree,
+        # it seems this is more easy to understand?
+        for i in range(index, len(nums)):
+            # attention it is i + 1
+            if self._recursion_1(nums, i+1, sum_total-nums[i]) is True:
+                return True
+        return False
+
+    def _recursion_1_memo(self, nums, index, sum_total, memo):
+        if sum_total == 0:
+            return True
+
+        if index >= len(nums) or sum_total < 0:
+            # could not fill the package
+            return False
+
+        if memo[index][sum_total] is not None:
+            return memo[index][sum_total]
+        # todo: usage of index, how about a for loop?
+        for i in range(index, len(nums)):
+            if self._recursion_1_memo(nums, i+1, sum_total-nums[i], memo) is True:
+                memo[index][sum_total] = True
+                return True
+        memo[index][sum_total] = False
+        return False
 
     def _recursion_memo(self, nums, index, sum_total, memo):
         # the diff parameters is index and sum_total, so a 2D list is needed
@@ -99,7 +141,9 @@ class Solution(object):
 if __name__ == "__main__":
     test_nums = [1, 5, 11, 5]
     # test_nums = [1, 2, 3, 5]
-    test_nums = [1, 1]
+    # test_nums = [1, 1]
+    test_nums = [1,2,5]
+    test_nums = [2,2,3,5]
     print(Solution().canPartition(test_nums))
 
 
